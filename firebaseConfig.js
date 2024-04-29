08fc990c636a4d15adf53b8d4e89abecc0fd07d5
shvalin1 <shvalin01@gmail.com>
2024-04-29 21:54:26 +0900

    firebaseConfigを編集

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,11 +1,11 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import Constants from "expo-constants";
 
 // Initialize Firebase
 let app;
-if (!initializeApp.length) {
+if (getApps().length === 0) {
   app = initializeApp(Constants.expoConfig.extra.firebase);
 }
 
